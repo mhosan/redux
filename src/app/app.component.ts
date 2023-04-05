c05725f7f63e16f4752245cfd42d27b07d757cda
Sergio Marcelo Hosan <marcelo.hosan@arba.gov.ar>
2023-04-05 18:22:19 -0300

    viendo el estado de un solo nodo e implementando el reducer con la libreria ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,21 @@ export class AppComponent {
   contador!: number;
 
   constructor( private store: Store<AppState>){
-    this.store.subscribe(state =>{
-      console.log(state);
-      this.contador = state.contador;
-    });
+    //con esta subscripcion estoy escuchando todo. El state o el compomente puede tener
+    //muchas ramificaciones y de este modo estoy escuchando todo
+    //this.store.subscribe(state =>{
+    //  console.log(state);
+    //  this.contador = state.contador;
+    //});
+
+    //pero puedo escuchar algo especificamente e ignorar todo lo demas. Para esto el store
+    //tiene un metodo "select" para seleccionar el nodo que quiero escuchar. En este caso 
+    //seria el contador:
+    this.store.select('contador').subscribe(contador =>{
+      console.log(`contador: ${contador}`);
+      this.contador = contador;
+    })
+
   }
 
   incrementar(){
--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,7 +1,7 @@
-import { Action } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
 import { incrementar, decrementar } from './contador.actions';
 
-export function contadorReducer (state: number = 10, action: Action) {
+/* export function contadorReducer (state: number = 10, action: Action) {
     switch(action.type){
         case incrementar.type:
             return state + 1;
@@ -11,4 +11,16 @@ export function contadorReducer (state: number = 10, action: Action) {
         default:
             return state;
     }
+} */
+
+//el código anterior es una forma manual de armar un reducer, pero hay herramientas de la libreria
+//ngrx que son mas simples y eficientes:
+export const initialState = 20;
+const _contadorReducer =createReducer(initialState,
+    on(incrementar, state => state + 1),
+    on(decrementar, state => state - 1)
+);
+
+export function contadorReducer(state:any, action:any) {
+    return _contadorReducer(state, action);
 }
